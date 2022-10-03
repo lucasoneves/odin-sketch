782d2eeec123d6bb947703f0ddfb6b3cdc083f97
Lucas Neves <lucasdeoliveiraneves@Mac-mini-de-Lucas.local>
2022-10-03 00:23:38 -0300

    Iterando e adicionando todas as divs

---
+++ index.js
@@ -0,0 +1,7 @@
+const mainDiv = document.querySelector(".content");
+const divText = document.createElement('div');
+divText.textContent = 'lucas'
+
+for (let i = 0; i < 256; i++) {
+    mainDiv.innerHTML += divText.textContent = 'lucas'
+}
